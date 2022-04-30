b1abb86f8c2cbd089dd13a92d25eb2cb7b99ac57
birros <birros@protonmail.com>
2022-04-30 02:41:54 +0200

    fix(utils): fix missing arch

--- lib/utils.bash
+++ lib/utils.bash
@@ -61,7 +61,7 @@ download_release() {
       ;;
   esac
 
-  url="$GH_REPO/releases/download/version_${version}/binaryen-version_${version}-${platform}.tar.gz"
+  url="$GH_REPO/releases/download/version_${version}/binaryen-version_${version}-${arch}-${platform}.tar.gz"
 
   echo "* Downloading $TOOL_NAME release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"

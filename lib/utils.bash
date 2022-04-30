d40f3b12e85d015ffe1b82a6a35c5208f1f565fd
birros <birros@protonmail.com>
2022-04-30 03:02:43 +0200

    fix(utils): use releases only

--- lib/utils.bash
+++ lib/utils.bash
@@ -24,9 +24,8 @@ sort_versions() {
 }
 
 list_github_tags() {
-  git ls-remote --tags --refs "$GH_REPO" |
-    grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    grep version_ | sed 's/^version_//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+  curl -s https://api.github.com/repos/WebAssembly/binaryen/releases |
+  grep tag_name | grep version_ | sed -r 's/^    "tag_name": "version_(.+)",$/\1/'
 }
 
 list_all_versions() {
